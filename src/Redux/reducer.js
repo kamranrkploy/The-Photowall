bc59ba4100797824ca32a24263b8583b50d01451
kamranrk <kamran16alam@gmail.com>
2020-07-28 20:52:36 +0530

    started implementing the app through redux and created Methods.txt

---
+++ src/Redux/reducer.js
@@ -0,0 +1,9 @@
+const { func } = require("prop-types");
+import posts from '../Data/posts'
+
+const PostReducer = function Posts(state=posts , action){
+    return state
+}
+
+
+export default PostReducer
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDom from 'react-dom';
 import Main from './Components/Main';
 import './style/style.css';
 import {HashRouter} from 'react-router-dom';
+import {createStore} from 'redux';
+import rootReducer from './Redux/reducer';
+
+const store = createStore(rootReducer)
+
 
 //whenever the instance of main class is called its constructor will automatically be called
 
