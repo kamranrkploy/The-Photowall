6357abc4305ecb38ad4406c82d78ba3e684a9fb9
kamranrk <kamran16alam@gmail.com>
2020-07-31 19:46:41 +0530

    Database creation started

---
+++ src/database/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+
+//using CDN
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDlOPAWWtrB1vWg17d3uPWjLxRleh9oCDA",
+  authDomain: "thephotowallapp.firebaseapp.com",
+  databaseURL: "https://thephotowallapp.firebaseio.com",
+  projectId: "thephotowallapp",
+  storageBucket: "thephotowallapp.appspot.com",
+  messagingSenderId: "582655079973",
+  appId: "1:582655079973:web:e352f12f39739701a171cc",
+  measurementId: "G-754C3MRNWZ"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const database = firebase.database()
+
+export {database}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './style/style.css';
 import {HashRouter} from 'react-router-dom';
-import {createStore} from 'redux';
+import {createStore ,applyMiddleware} from 'redux';
 import rootReducer from './Redux/reducer';
 import {Provider} from 'react-redux';
 import App from './Components/App';
+import thunk from 'redux-thunk';
+import {database} from './database/config'
 
-
-const store = createStore(rootReducer , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(rootReducer , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
 
 
 //whenever the instance of main class is called its constructor will automatically be called
