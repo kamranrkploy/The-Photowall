64f3f4c89303d8467e889cb4575e8390fea764e5
kamranrk <kamran16alam@gmail.com>
2020-07-31 17:56:12 +0530

    added hashrouter for github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './style/style.css';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {createStore} from 'redux';
 import rootReducer from './Redux/reducer';
 import {Provider} from 'react-redux';
@@ -13,4 +13,4 @@ const store = createStore(rootReducer , window.__REDUX_DEVTOOLS_EXTENSION__ && w
 
 //whenever the instance of main class is called its constructor will automatically be called
 
-ReactDom.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider> , document.getElementById('root'));
+ReactDom.render(<Provider store={store}><HashRouter><App/></HashRouter></Provider> , document.getElementById('root'));
