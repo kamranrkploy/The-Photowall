5b0069b322f23e94778db1871752423c5d037207
kamranrk <kamran16alam@gmail.com>
2020-08-03 20:58:59 +0530

    Updates

--- precache-manifest.e6d37aa8a9cd187460050bfbcc76424e.js
+++ precache-manifest.e6d37aa8a9cd187460050bfbcc76424e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b292dd63a7989e7c07e746e62f7b5097",
+    "revision": "f7a95c21af8228dfa957279d56698e98",
     "url": "/The-Photowall-App/index.html"
   },
   {
-    "revision": "634fcbdabd31912d11bd",
-    "url": "/The-Photowall-App/static/css/main.82a6b97d.chunk.css"
+    "revision": "dc8d361de925c425a8f4",
+    "url": "/The-Photowall-App/static/css/main.e1cab19c.chunk.css"
   },
   {
     "revision": "efa4d9f57f1476ff01a9",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/The-Photowall-App/static/js/2.972d197d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "634fcbdabd31912d11bd",
-    "url": "/The-Photowall-App/static/js/main.3922ff58.chunk.js"
+    "revision": "dc8d361de925c425a8f4",
+    "url": "/The-Photowall-App/static/js/main.b44c9575.chunk.js"
   },
   {
     "revision": "cd69f4387cbb19f605e3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/The-Photowall-App/precache-manifest.25185905f5451e4cfd69a2c6c8d6019c.js"
+  "/The-Photowall-App/precache-manifest.e6d37aa8a9cd187460050bfbcc76424e.js"
 );
 
 self.addEventListener('message', (event) => {
